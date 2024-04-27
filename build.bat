e4f626f84812b9db0f360ab38b8f25a168cc17e6
EyeDaleHim <71162374+EyeDaleHim@users.noreply.github.com>
2024-04-27 17:18:36 +0800

    opengl code

--- build.bat
+++ build.bat
@@ -2,3 +2,5 @@
 cls
 set VSLANG=1033
 haxe.exe build.hxml
+cd export
+start Global.exe
--- src/Global.hx
+++ src/Global.hx
@@ -1,6 +1,7 @@
 package;
 
 import glfw.GLFW.*;
+import opengl.GL.*;
 
 class Global {
 	private static var _lastTick:Float = 0.0;
@@ -10,14 +11,17 @@ class Global {
 			var window = glfwCreateWindow(640, 480, "Hello World", null, null);
 			glfwMakeContextCurrent(window);
 
-			/* Loop until the user closes the window */
 			while (glfwWindowShouldClose(window) != GLFW_TRUE) {
-				Sys.println('${glfwGetTime() - _lastTick}');
-				_lastTick = glfwGetTime();
-				/* Swap front and back buffers */
+				// var width:Int = 0;
+				// var height:Int = 0;
+
+				// glfwGetFramebufferSize(window, cpp.Pointer.addressOf(width), cpp.Pointer.addressOf(height));
+
+				glClearColor(0.0, 0.0, 0.0, 1.0);
+				glClear(GL_COLOR_BUFFER_BIT);
+
 				glfwSwapBuffers(window);
 
-				/* Poll for and process events */
 				glfwPollEvents();
 			}
 
